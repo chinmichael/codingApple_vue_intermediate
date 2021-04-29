7d6f6982156f0f6cc26393060bfad86a5d5f8ea1
chinmichael <chinmichael@naver.com>
2021-04-29 16:49:43 +0900

    vue study 10th day:bootstrap-npm-setting, review

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,103 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <!--와이드 container-->
+        <a class="navbar-brand" href="#">Vue Study</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">List</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Detail</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="mt-4 p-4 text-center intro-box">
+        <h5>Vue 공부용 페이지입니다</h5>
+        <p>-Vue.js로 제작함-</p>
+      </div>
+      <List v-bind:list_item="list_inform"></List>
+    </div>
   </div>
+  <!--The template root requires exactly one element 막기 위해 하나의 div로 감쌈-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from "./components/List.vue";
+import { list_inform } from "./assets/content_list";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      list_inform: list_inform,
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    List: List,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+div,
+p,
+h5 {
+  box-sizing: border-box;
+}
+.text-center {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.intro-box {
+  background-color: white;
+  border-radius: 5px;
+}
+.intro-box p {
+  margin: 0;
 }
 </style>
+
+<!--4/29 vue 강의 2단계 시작 : 까먹진 않았겠지 vue create 프로젝트
+                               >> 목표 : 라우터(페이지를 나눠서 이동시키기 위함)
+
+    4/29 : vue에서 bootstrap 라이브러리 사용 + 데이터바인딩, props 복습
+
+    1. 부트스트랩 설치(참고로 부트스트랩 5버전은 explorer 아예 안됨)
+    case 1) index.html에 기존처럼 CDN을(css, js) 첨부하던 파일 저장 후 저장 경로를 통해 첨부하던 하면 됨
+
+    case 2) npm으로 설치
+    (npm(node package manager) : JS프로그래밍 언어를 위한 패키지 관리자 / JS 런타임환경 Node.js의 기본 패키지 관리자 (그래서 Node.js설치하는거))
+    터미널에서(해당 프로젝트 폴더에서 해야함!! + npm run serve 등의 작업을 하던 node 터미널을 끄고 powershell터미널로 해야함)
+
+    bootstrap 5버전 : npm install bootstrap@next @popperjs/core
+              4버전 : npm install bootstrap jquery popper.js
+
+    main.js에 2가지를 import (import 'bootstrap'  import 'bootstrap/dist/css/bootstrap.min.css')
+
+    정확한 명령어는(바뀔 수 있으므로) bootstrap 홈페이지의 getting start의 download의 npm파트 (yarn은 yarn add bootstrap@next)
+-->
+
+<!-- 4/29 : 데이터 가져오기 연습
+     asset에 js파일로 리스트 저장(강의에서 제공)
+     export default = []로 가도 좋고
+     let 변수명으로 지정하고 위에 export{변수명}으로 해서 데이터 세팅
+
+     데이터를 가져오고자 하는 컴포넌트에 import {export한 변수명} from 경로
+     data(){}에 변수명: export한 데이터 변수명 대입
+-->
---
+++ src/components/List.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="container mt-4 mb-0 p-1 list-head">
+      <p class="text-center">Content List</p>
+    </div>
+    <div class="container mt-0 pl-4 pr-4 pb-4 list-body">
+      <div
+        class="content-list-box mt-4 p-4 text-center"
+        v-for="(a, i) in list_item"
+        :key="i"
+      >
+        <h5>{{ a.title }}</h5>
+        <p>{{ a.date }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: {
+    list_item: Array,
+  },
+};
+</script>
+
+<style>
+.text-center {
+  text-align: center;
+}
+
+.list-head {
+  background-color: darkgray;
+  border: 1px solid darkgray;
+}
+.list-head p {
+  color: white;
+  font-size: 18px;
+  font-weight: bold;
+  margin: 0;
+}
+.list-body {
+  border: 1px solid darkgray;
+}
+
+.content-list-box {
+  background-color: white;
+  border-radius: 5px;
+}
+
+.content-list-box h5 {
+  margin-bottom: 10px;
+  font-size: 20px;
+  font-weight: 600;
+  color: olivedrab;
+}
+
+.content-list-box p {
+  margin: 0;
+  font-size: 15px;
+  font-weight: 600;
+  color: lightgrey;
+}
+</style>
+
+<!-- 4/29 : 컴포넌트 추가 복습
+     components 폴더 등에 .vue 파일 만들고
+     export default{}에 name 세팅 (이건 그냥 되도록)
+
+     보이고자 하는 컴포넌트(App.vue)에
+     import에 name과 경로로 컴포넌트 로드
+     components{}에 변수명과 대입 컴포넌트 작성
+-->
+
+<!-- 4/29 : props연습
+     상위 컴포넌트에서 컴포넌트 프로퍼티에
+     v-bind:를 통해 props변수명="data변수명"으로 받아옴
+
+     export {} 안 props:{}에 받아옴 props변수명:자료형으로 세팅
+-->
